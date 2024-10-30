30152f82f6d240ea3262f8cfe5451fa33acdf2b1
iri <68111122+Bryan051@users.noreply.github.com>
2024-10-30 21:49:52 +0900

    [Silver II] Title: N과 M (12), Time: 116 ms, Memory: 14816 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.java
@@ -0,0 +1,57 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    static int N, M;
+    static int[] arr;
+    static int[] result;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[N];
+        result = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+        dfs(0);
+        System.out.println(sb);
+
+
+    }
+
+    public static void dfs(int depth) {
+        if (depth == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(result[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        int previousNumber = -1;
+        for (int i = 0; i < N; i++) {
+            if (previousNumber == arr[i]) continue;
+            result[depth] = arr[i];
+            if (depth > 0 && result[depth - 1] > result[depth]) continue;
+            dfs(depth + 1);
+            previousNumber = arr[i];
+        }
+
+    }
+
+
+}
