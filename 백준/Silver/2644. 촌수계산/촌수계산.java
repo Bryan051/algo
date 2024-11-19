8728034af615d2ef59b50c1a7abc7629422b8922
iri <68111122+Bryan051@users.noreply.github.com>
2024-11-19 21:21:03 +0900

    [Silver II] Title: 촌수계산, Time: 100 ms, Memory: 14148 KB -BaekjoonHub

--- 백준/Silver/2644. 촌수계산/촌수계산.java
+++ 백준/Silver/2644. 촌수계산/촌수계산.java
@@ -16,19 +16,17 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-        StringBuilder sb = new StringBuilder();
 
-        // 전체 사람 node의 수 N
-        st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        // 계산해야하는 번호 두개 a,b
+        // 전체 사람(node) 수
+        int n = Integer.parseInt(br.readLine());
+
+        // 촌수를 계산해야 하는 서로다른 두 사람의 번호
         st = new StringTokenizer(br.readLine());
         int a = Integer.parseInt(st.nextToken());
         int b = Integer.parseInt(st.nextToken());
-        // 부모자식관계 갯수 m = edge 간선
 
-        st = new StringTokenizer(br.readLine());
-        int m = Integer.parseInt(st.nextToken());
+        // 부모 자식들간의 관계의 갯수 m (edge, 간선)
+        int m = Integer.parseInt(br.readLine());
 
         dist = new int[n + 1];
         visited = new boolean[n + 1];
@@ -37,43 +35,46 @@ public class Main {
         for (int i = 0; i <= n; i++) {
             graph.add(new ArrayList<Integer>());
         }
-        // 간선 갯수 m 만큼 . 무방향 이므로 x,y 둘다. x가 y의 부모
+
+        // 간선 갯수 m 만큼. 무방향. x,y 둘다. x 가 y 의 부모.
         for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
             int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
-
             graph.get(x).add(y);
             graph.get(y).add(x);
         }
 
-        int ans = bfs(a,b); // 시작,끝
+        int ans = bfs(a, b);
         System.out.println(ans);
 
     }
 
-    private static int bfs(int start, int end) {
-        Queue<Integer> q = new LinkedList<>();
+    static int bfs(int start, int end) {
+        Queue<Integer> queue = new LinkedList<>();
         dist[start] = 0;
         visited[start] = true;
+        queue.add(start);
 
-        q.offer(start);
-        while (!q.isEmpty()) {
-            int cur = q.poll();
+        while (!queue.isEmpty()) {
+            int cur = queue.poll();
 
             for (int i = 0; i < graph.get(cur).size(); i++) {
                 int next = graph.get(cur).get(i);
 
                 if (!visited[next]) {
-                    dist[next] = dist[cur]+1;
+                    dist[next] = dist[cur] + 1;
                     if (next == end) {
                         return dist[next];
                     }
-                    q.offer(next);
+                    queue.add(next);
                     visited[next] = true;
                 }
+
             }
         }
         return -1;
     }
+
+
 }
